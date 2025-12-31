d59cb8a19468f86cef3307976dfc85c8479a06af
Ninad Shirsat <ninad@192.168.1.3>
2025-12-31 15:01:30 +0530

    fixed css changes

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -43,7 +43,7 @@ const NoteForm = ({ notes, setNotes }) => {
       {/* Toggle Button */}
       <button
         onClick={() => setIsFormVisible(!isFormVisible)}
-        className="w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-200 hover: border-purple-300 transition mb-4"
+        className="w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-200 hover:border-purple-300 transition mb-4"
       >
         {isFormVisible ? "Hide Form ✖️" : "Add New Note ➕"}
       </button>
@@ -96,7 +96,7 @@ const NoteForm = ({ notes, setNotes }) => {
             required
           />
 
-          <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover : bg-purple-600">
+          <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600">
             Add Note
           </button>
         </form>
